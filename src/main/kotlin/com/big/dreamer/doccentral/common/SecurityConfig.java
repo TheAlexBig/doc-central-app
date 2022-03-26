db451f93ac6aa24eba8767ec58dde72e6caf6f3b
@jose.valladares <jvalladares@applaudostudios.com>
2022-03-26 11:18:04 -0600

    Supporting cors for doc-central

---
+++ src/main/kotlin/com/big/dreamer/doccentral/common/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.big.dreamer.doccentral.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+@Slf4j
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Value("${config.request}")
+    private String website;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        log.info("Allowed origin [{}]", website);
+        configuration.setAllowedOrigins(List.of(website));
+        configuration.setAllowedMethods(List.of("HEAD",
+                "GET", "POST", "PUT", "DELETE", "PATCH"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
