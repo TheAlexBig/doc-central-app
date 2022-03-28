b62f0164f497b05ae6447a537e41782cd8813893
@jose.valladares <jvalladares@applaudostudios.com>
2022-03-27 18:15:30 -0600

    Remove csrf

--- src/main/kotlin/com/big/dreamer/doccentral/common/SecurityConfig.java
+++ src/main/kotlin/com/big/dreamer/doccentral/common/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -12,23 +13,26 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
 
-@Configuration
+
 @Slf4j
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Value("${config.request}")
-    private String website;
+    private String origin;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors();
+        http.cors().and()
+                .csrf().disable();
     }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         final CorsConfiguration configuration = new CorsConfiguration();
-        log.info("Allowed origin [{}]", website);
-        configuration.setAllowedOrigins(List.of(website));
-        configuration.setAllowedMethods(List.of("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH"));
+        log.info("Allowed origin [{}]", origin);
+        configuration.setAllowedOrigins(List.of(origin));
+        configuration.setAllowedMethods(List.of("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type", "Content-disposition"));
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
